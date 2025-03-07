6793d213ae79650f8ce0316e66a23e2d8906adf8
dadayama <yamaweb1124@gmail.com>
2025-03-08 00:44:23 +0900

    feat:認証機能の実装

--- components/RecipeList/RecipeList.tsx
+++ components/RecipeList/RecipeList.tsx
@@ -1,37 +1,21 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import RecipeCard from '../RecipeCard/RecipeCard';
 
 type Props = {
   title: 'string';
   posts: any;
 };
 
-const RecipeList = ({ title, posts }: Props) => {
+const RecipeList = ({ posts }: Props) => {
   return (
     <>
-      <section>
-        <h2>{title}</h2>
-        <ul>
-          {posts?.map((post) => (
-            <li key={post.id}>
-              <Link href={`recipe/${post.id}`}>
-                <h3>{post.title}</h3>
-                <div>
-                  <Image
-                    src={post.img}
-                    width={100}
-                    height={100}
-                    alt='仮のalt'
-                  />
-                </div>
-                <p>{post.comments}</p>
-                <p>材料：{post.ingredients}</p>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <ul>
+        {posts?.map((post) => (
+          <RecipeCard post={post} />
+        ))}
+      </ul>
     </>
   );
 };
