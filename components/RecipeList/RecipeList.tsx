db77c4b1bf1fe6d67145647d01b01e5a70cf6cd9
dadayama <yamaweb1124@gmail.com>
2025-03-07 23:59:54 +0900

    feat:レシピリストの作成

---
+++ components/RecipeList/RecipeList.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+type Props = {
+  title: 'string';
+  posts: any;
+};
+
+const RecipeList = ({ title, posts }: Props) => {
+  return (
+    <>
+      <section>
+        <h2>{title}</h2>
+        <ul>
+          {posts?.map((post: any) => (
+            <li key={post.id}>
+              <Link href={`recipe/${post.id}`}>
+                <h3>{post.title}</h3>
+                <div>
+                  <Image
+                    src={post.img}
+                    width={100}
+                    height={100}
+                    alt='仮のalt'
+                  />
+                </div>
+                <p>{posts.comments}</p>
+                <p>材料：{post.ingredients}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default RecipeList;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsparser,
+    },
+    plugins: {
+      "@typescript-eslint":tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any":"warn",
+    }
+  }
 ];
 
 export default eslintConfig;
