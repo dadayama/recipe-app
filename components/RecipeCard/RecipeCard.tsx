e1d768096f937ff3514387f5f0a78ad724ec9dec
dadayama <yamaweb1124@gmail.com>
2025-03-09 00:34:38 +0900

    feat:next.config.tsにfirebasestorageのドメインを設定して外部の画像が読み込まれるように変更

--- components/RecipeCard/RecipeCard.tsx
+++ components/RecipeCard/RecipeCard.tsx
@@ -3,15 +3,23 @@ import Link from 'next/link';
 import React from 'react';
 
 const RecipeCard = (post) => {
+  console.log('post', post.post.img);
+
   return (
     <li key={post.id}>
-      <Link href={`recipe/${post.id}`}>
-        <h3>{post.title}</h3>
+      <Link href={`recipe/${post.post.id}`}>
+        <h3>{post.post.title}</h3>
         <div>
-          <Image src={post.img} width={100} height={100} alt='仮のalt' />
+          {/* <img src={post.post.img || null} alt='Description' /> */}
+          <Image
+            src={post.post.img || null}
+            alt='Description'
+            width={300}
+            height={200}
+          />
         </div>
-        <p>{post.comments}</p>
-        <p>材料：{post.ingredients}</p>
+        <p>{post.post.comments}</p>
+        <p>材料：{post.post.ingredients}</p>
       </Link>
     </li>
   );
